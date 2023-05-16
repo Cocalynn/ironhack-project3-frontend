526006fcc6690dc0ae6e4dbc0285a3c9099ce625
YinongLi <yinong.li97@gmail.com>
2023-05-16 20:40:38 +0100

    build: show user pic in frontend

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import './Home.css'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import cognitoUtils from '../lib/cognitoUtils'
 import request from 'request'
@@ -18,7 +19,7 @@ class Home extends Component {
   componentDidMount () {
     if (this.props.session.isLoggedIn) {
       // Call the API server GET /users endpoint with our JWT access token
-      const options = {
+      const option1 = {
         url: `${appConfig.apiUri}/users`,
         headers: {
           Authorization: `Bearer ${this.props.session.credentials.accessToken}`
@@ -26,7 +27,7 @@ class Home extends Component {
       }
 
       this.setState({ apiStatus: 'Loading...' })
-      request.get(options, (err, resp, body) => {
+      request.get(option1, (err, resp, body) => {
         let apiStatus, apiResponse
         if (err) {
           // is API server started and reachable?
@@ -43,12 +44,64 @@ class Home extends Component {
         }
         this.setState({ apiStatus, apiResponse })
       })
+
+
+      // Call the API server GET /user endpoint with our JWT access token
+      const option2 = {
+        url: `${appConfig.apiUri}/user`,
+        headers: {
+          Authorization: `Bearer ${this.props.session.credentials.accessToken}`
+        }
+      }
+      
+      request.get(option2, (err, resp, body) => {
+        let user, profileImage
+        if (err) {
+          // is API server started and reachable?
+          console.error('Unable to reach API: ' + err)
+        }
+        else if (resp.statusCode !== 200) {
+          // API returned an error
+          console.error('Error response received: ' + JSON.stringify(resp))
+        }
+        else {
+          user = body // user is a string
+          profileImage = JSON.parse(user).profileImage
+        }
+        this.setState({ user, profileImage })
+      })
     }
   }
+
+  // getUser = (e) => {
+  //   e.preventDefault()
+  //   const [user, setUser] = useState(null)
+  //   axios.get(`${appConfig.apiUri}/user`)
+  //     .then((response) => {
+  //       const currentUser = response.data
+  //       setUser(currentUser)
+  //       console.log(user)
+  //     })
+  //     .catch((error) => {
+  //       console.log(error)
+  //     })
+  // }
+
+  // useEffect(() => {
+  //   getUser()
+  // }, []);
 
   onSignOut = (e) => {
     e.preventDefault()
     cognitoUtils.signOutCognitoSession()
+  }
+
+  changeNickname = (e) => {
+    console.log('changeNickname')
+  }
+
+  uploadProfilePicture = (e) => {
+    console.log('uploadProfilePicture')
   }
 
   render () {
@@ -57,8 +110,12 @@ class Home extends Component {
         <header className="Home-header">
           { this.props.session.isLoggedIn ? (
             <div>
-              <p>You are logged in as user {this.props.session.user.userName} ({this.props.session.user.email}).</p>
-              <p></p>
+              <p>Welcome! {this.props.session.user.userName}</p>
+              <img src={this.state.profileImage}/>
+              <p>{this.props.session.user.email}</p>
+              <button onClick={this.changeNickname}>Change my nickname</button>
+              <button onClick={this.uploadProfilePicture}>Upload profile picture</button>
+              <hr></hr>
               <div>
                 <div>API status: {this.state.apiStatus}</div>
                 <div className="Home-api-response">{this.state.apiResponse}</div>
