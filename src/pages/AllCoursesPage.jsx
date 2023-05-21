a34a30f9c103a1ffd287e4a3e3d339a1d7575e5f
Wilkins Morales <wilkinscom@gmail.com>
2023-05-21 18:18:33 +0200

    fix course create

--- src/pages/AllCoursesPage.jsx
+++ src/pages/AllCoursesPage.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
-import { Spinner } from "react-bootstrap";
-import CourseSearch from "../components/CourseSearch";
-import appConfig from "../config/app-config.json";
+import { Link as RouterLink } from "react-router-dom";
 import { useSelector } from "react-redux";
-import FootBar from '../components/FootBar';
-
+import appConfig from "../config/app-config.json";
+import CourseSearch from "../components/CourseSearch";
+import FootBar from "../components/FootBar";
+import {
+  Button,
+  Box,
+  Grid,
+  Card,
+  CardContent,
+  Typography,
+  CircularProgress,
+  Link,
+} from "@mui/material";
 
 const AllCoursesPage = () => {
   const session = useSelector((state) => state.session);
@@ -40,32 +48,75 @@ const AllCoursesPage = () => {
   }, []);
 
   if (isLoading) {
-    return <Spinner animation="border" />;
+    return (
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        minHeight="50vh"
+      >
+        <CircularProgress />
+      </Box>
+    );
   }
 
   return (
-    <div className="container">
-      <div className="row">
+    <Box
+      sx={{
+        flexGrow: 1,
+        padding: 3,
+        paddingBottom: "60px",
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+      }}
+    >
+      <Box>
         <CourseSearch setCourses={setCourses} />
-        {courses.length === 0 && <h3>No courses found</h3>}
-        {courses.map((course) => (
-          <div key={course._id} className="col-md-4 mb-4">
-            <div className="card">
-              <div className="card-body">
-                <Link to={`/courses/${course._id}`}>
-                  <h5 className="card-title">{course.name}</h5>
-                </Link>
-                <p className="card-text">{course.description}</p>
-              </div>
-              <div className="card-footer">
-                <p className="card-text">{course.price}€</p>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
+        <Button component={RouterLink} to="/add-course" variant="contained">
+          Add New Course
+        </Button>
+        {courses.length === 0 ? (
+          <Typography variant="h5" component="div">
+            No courses found
+          </Typography>
+        ) : (
+          <Grid container spacing={3}>
+            {courses.map((course) => (
+              <Grid item key={course._id} xs={12} sm={6} md={4}>
+                <Card>
+                  <CardContent>
+                    <Link
+                      component={RouterLink}
+                      to={`/courses/${course._id}`}
+                      variant="body2"
+                    >
+                      <Typography
+                        variant="h6"
+                        component="div"
+                        fontWeight="bold"
+                      >
+                        {course.name}
+                      </Typography>
+                    </Link>
+                    <Typography variant="body2" color="text.secondary">
+                      {course.description}
+                    </Typography>
+                  </CardContent>
+                  <Box sx={{ p: 2, textAlign: "center" }}>
+                    <Typography variant="body2" color="text.secondary">
+                      {course.price}€
+                    </Typography>
+                  </Box>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        )}
+      </Box>
       <FootBar />
-    </div>
+    </Box>
   );
 };
 
